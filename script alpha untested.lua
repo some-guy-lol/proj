5111dc61f5951d99969719bbf61582713212a4f5
some-guy-lol <124967226+some-guy-lol@users.noreply.github.com>
2023-02-14 18:31:10 -0600

    Update script alpha untested.lua

--- script alpha untested.lua
+++ script alpha untested.lua
@@ -1,137 +1,121 @@
 local market = game:GetService("Players").LocalPlayer.PlrData:WaitForChild("MarketUses")
-local plr = game:GetService("Players").LocalPlayer
-local coregui = game:GetService("CoreGui")
-local bmuseui = coregui.bmUseUi
-local checkInvButton = coregui.bmUseUi.Frame.TextButton
-local textbox = bmuseui.Frame.TextBox
-local notifLabel = bmuseui.Notif
-local textboxEntry = textbox.Text
 
 
 
--- GUI Ends on line 126
--- Gui to Lua
+
+-- GUI ends at line 98
 -- Version: 3.2
 
 -- Instances:
 
-local bmUseUI = Instance.new("ScreenGui")
+local bmUseUi = Instance.new("ScreenGui")
 local TextLabel = Instance.new("TextLabel")
 local UIAspectRatioConstraint = Instance.new("UIAspectRatioConstraint")
 local UITextSizeConstraint = Instance.new("UITextSizeConstraint")
-local Frame = Instance.new("Frame")
-local TextLabel_2 = Instance.new("TextLabel")
-local UITextSizeConstraint_2 = Instance.new("UITextSizeConstraint")
-local TextBox = Instance.new("TextBox")
-local UITextSizeConstraint_3 = Instance.new("UITextSizeConstraint")
-local TextButton = Instance.new("TextButton")
-local UITextSizeConstraint_4 = Instance.new("UITextSizeConstraint")
+local Frame = Instance.new("ImageLabel")
 local UIAspectRatioConstraint_2 = Instance.new("UIAspectRatioConstraint")
-local Notif = Instance.new("TextLabel")
+local TextBox = Instance.new("TextBox")
 local UIAspectRatioConstraint_3 = Instance.new("UIAspectRatioConstraint")
-local UITextSizeConstraint_5 = Instance.new("UITextSizeConstraint")
+local UITextSizeConstraint_2 = Instance.new("UITextSizeConstraint")
+local TextButton = Instance.new("TextButton")
+local UIAspectRatioConstraint_4 = Instance.new("UIAspectRatioConstraint")
+local UITextSizeConstraint_3 = Instance.new("UITextSizeConstraint")
 
 --Properties:
 
-bmUseUI.Name = "bmUseUI"
-bmUseUI.Parent = game:GetService("CoreGui")
-bmUseUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+bmUseUi.Name = "bmUseUi"
+bmUseUi.Parent = game:GetService("CoreGui")
 
-TextLabel.Parent = bmUseUI
-TextLabel.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.Parent = bmUseUi
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.BackgroundTransparency = 1.000
-TextLabel.Position = UDim2.new(0.404202104, 0, 0, 0)
-TextLabel.Size = UDim2.new(0, 368, 0, 92)
+TextLabel.Position = UDim2.new(0.088044025, 0, 0.949186981, 0)
+TextLabel.Size = UDim2.new(0, 154, 0, 50)
 TextLabel.Font = Enum.Font.SourceSans
-TextLabel.Text = "bmUseTime"
+TextLabel.Text = "bmUses"
 TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextLabel.TextScaled = true
 TextLabel.TextSize = 14.000
 TextLabel.TextWrapped = true
 
 UIAspectRatioConstraint.Parent = TextLabel
-UIAspectRatioConstraint.AspectRatio = 4.000
+UIAspectRatioConstraint.AspectRatio = 3.080
 
 UITextSizeConstraint.Parent = TextLabel
-UITextSizeConstraint.MaxTextSize = 91
+UITextSizeConstraint.MaxTextSize = 50
 
-Frame.Parent = bmUseUI
-Frame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-Frame.BackgroundTransparency = 0.350
-Frame.Position = UDim2.new(0, 0, 0.866869926, 0)
-Frame.Size = UDim2.new(0, 321, 0, 131)
+Frame.Name = "Frame"
+Frame.Parent = bmUseUi
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.BackgroundTransparency = 1.000
+Frame.Position = UDim2.new(0, 0, 0.898373961, 0)
+Frame.Size = UDim2.new(0, 330, 0, 100)
+Frame.Image = "rbxassetid://3570695787"
+Frame.ImageColor3 = Color3.fromRGB(0, 0, 0)
+Frame.ImageTransparency = 0.350
+Frame.ScaleType = Enum.ScaleType.Slice
+Frame.SliceCenter = Rect.new(100, 100, 100, 100)
+Frame.SliceScale = 0.120
 
-TextLabel_2.Parent = Frame
-TextLabel_2.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel_2.BackgroundTransparency = 1.000
-TextLabel_2.Size = UDim2.new(0, 321, 0, 50)
-TextLabel_2.Font = Enum.Font.SourceSans
-TextLabel_2.Text = "Plr Inventory Checker"
-TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel_2.TextScaled = true
-TextLabel_2.TextSize = 14.000
-TextLabel_2.TextWrapped = true
+UIAspectRatioConstraint_2.Parent = Frame
+UIAspectRatioConstraint_2.AspectRatio = 3.300
 
-UITextSizeConstraint_2.Parent = TextLabel_2
-UITextSizeConstraint_2.MaxTextSize = 43
-
-TextBox.Parent = Frame
-TextBox.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+TextBox.Parent = bmUseUi
+TextBox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextBox.BackgroundTransparency = 1.000
-TextBox.Position = UDim2.new(0, 0, 0.381679386, 0)
-TextBox.Size = UDim2.new(0, 200, 0, 50)
+TextBox.Position = UDim2.new(0, 0, 0.898373961, 0)
+TextBox.Size = UDim2.new(0, 176, 0, 50)
 TextBox.Font = Enum.Font.SourceSans
-TextBox.PlaceholderText = "Player Name"
+TextBox.PlaceholderText = "Plr Inv 2 Check"
 TextBox.Text = ""
 TextBox.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextBox.TextScaled = true
 TextBox.TextSize = 14.000
 TextBox.TextWrapped = true
-TextBox.ClearTextOnFocus = false
 
-UITextSizeConstraint_3.Parent = TextBox
-UITextSizeConstraint_3.MaxTextSize = 14
+UIAspectRatioConstraint_3.Parent = TextBox
+UIAspectRatioConstraint_3.AspectRatio = 3.520
 
-TextButton.Parent = Frame
+UITextSizeConstraint_2.Parent = TextBox
+UITextSizeConstraint_2.MaxTextSize = 34
+
+TextButton.Parent = bmUseUi
 TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextButton.BackgroundTransparency = 1.000
-TextButton.Position = UDim2.new(0.741433024, 0, 0.618320584, 0)
-TextButton.Size = UDim2.new(0, 83, 0, 50)
+TextButton.Position = UDim2.new(0.0935467705, 0, 0.897601485, 0)
+TextButton.Size = UDim2.new(0, 143, 0, 50)
 TextButton.Font = Enum.Font.SourceSans
-TextButton.Text = "Check"
+TextButton.Text = "Check Plr Inv"
 TextButton.TextColor3 = Color3.fromRGB(255, 255, 255)
 TextButton.TextScaled = true
 TextButton.TextSize = 14.000
 TextButton.TextWrapped = true
 
-UITextSizeConstraint_4.Parent = TextButton
-UITextSizeConstraint_4.MaxTextSize = 14
+UIAspectRatioConstraint_4.Parent = TextButton
+UIAspectRatioConstraint_4.AspectRatio = 2.860
 
-UIAspectRatioConstraint_2.Parent = Frame
-UIAspectRatioConstraint_2.AspectRatio = 2.450
+UITextSizeConstraint_3.Parent = TextButton
+UITextSizeConstraint_3.MaxTextSize = 32
+local Notif = Instance.new("TextLabel")
+
+--Properties:
 
 Notif.Name = "Notif"
-Notif.Parent = bmUseUI
+Notif.Parent = game:GetService("CoreGui").bmUseUi
 Notif.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
 Notif.BackgroundTransparency = 0.350
-Notif.Position = UDim2.new(0, 0, 0.621951222, 0)
-Notif.Size = UDim2.new(0, 200, 0, 50)
-Notif.Visible = false
+Notif.Position = UDim2.new(0.41120559, 0, 0.365313649, 0)
+Notif.Size = UDim2.new(0, 475, 0, 171)
 Notif.Font = Enum.Font.SourceSans
-Notif.Text = "Plr has weapon(s)!"
+Notif.Text = "plrItems"
 Notif.TextColor3 = Color3.fromRGB(255, 255, 255)
 Notif.TextScaled = true
 Notif.TextSize = 14.000
 Notif.TextWrapped = true
-
-UIAspectRatioConstraint_3.Parent = Notif
-UIAspectRatioConstraint_3.AspectRatio = 4.000
-
-UITextSizeConstraint_5.Parent = Notif
-UITextSizeConstraint_5.MaxTextSize = 14
+Notif.Visible = false
 
 -- GUI End
-
+local name2check = TextBox.Text
 -- functions
 local currentUses = nil
 local function checkMarketUsesVal()
@@ -140,24 +124,13 @@ end
 
 while wait(5) do
     checkMarketUsesVal()
-    game:GetService("CoreGui").ScreenGui.TextLabel.Text = 6-currentUses.."Up to 0 minutes left until the next BM use!"
+    game:GetService("CoreGui"):WaitForChild("bmUseUI").TextLabel.Text = 6-currentUses.."0 minutes left until the next BM use!"
 end
 
-checkInvButton.MouseButton1Down:Connect(function()
-    local plr2Check = textboxEntry
-    local found = game:GetService("Players"):FindFirstChild(textboxEntry)
-    local backpack = found:WaitForChild("Backpack")
-    if backpack:FindFirstChild("Ruger LCR .38") or backpack:FindFirstChild("M1911") or backpack:FindFirstChild("Colt .38 Special") or backpack:FindFirstChild("Taurus 856 .38") then
-        if backpack:FindFirstChild("Switchblade") then
-            notifLabel.Text = "Player has a switchblade!"
-            notifLabel.Visible = true
-            wait(3.5)
-            notifLabel.Visible = false
-            notifLabel.Text = "Plr has weapon(s)!"
-        else
-            notifLabel.Visible = true
-            wait(3.5)
-            notifLabel.Visible = false
-        end
-    end
+TextButton.MouseButton1Down:Connect(function()
+    local items = game.Players.name2check.Backpack:GetChildren()
+    Notif.Text = tostring(items)
+    Notif.Visible = true
+    wait(5)
+    Notif.Visible = false
 end)
